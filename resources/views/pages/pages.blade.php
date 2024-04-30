286367557b8778dfa96bbf756e8117185aa76f78
Jamil van de Ree <jgjvanderee@gmail.com>
2024-04-30 16:51:37 +0200

    Update pages.blade.php

--- resources/views/pages/pages.blade.php
+++ resources/views/pages/pages.blade.php
@@ -10,6 +10,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
                 @livewire('ui.pages.pages')
+                @livewire('toast')
             </div>
         </div>
     </div>
