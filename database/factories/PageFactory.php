66dc9f68bee49fadb5cf5bf22cc54578a981755d
Jamil van de Ree <jgjvanderee@gmail.com>
2024-05-03 15:50:56 +0200

    Update PageFactory.php

--- database/factories/PageFactory.php
+++ database/factories/PageFactory.php
@@ -6,32 +6,18 @@ use App\Models\Page;
 use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Page>
- */
 class PageFactory extends Factory
 {
-
-    /**
-     * The name of the factory's corresponding model
-     *
-     * @var string
-     */
     protected $model = Page::class;
 
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
+    public function definition()
     {
-        $title = $this->faker->sentence;
+        $title = $this->faker->realText(10);
 
         return [
             'title' => $title,
             'slug' => Str::slug($title),
-            'content' =>$this->faker->paragraph,
+            'content' => $this->faker->realText(50),
         ];
     }
 }
